4585596d421ca5aad071a29fdffb37e7e1638b93
jose.paulo <jose.paulo@irancho.com.br>
2021-11-02 19:45:36 -0300

    feat Add Nova rota update

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,6 @@ const rotas = Router();
 rotas.get("/registrar", cadastrarAnimais.index);
 rotas.get("/registrar/:id", cadastrarAnimais.show);
 rotas.post("/registrar", cadastrarAnimais.stoge);
+rotas.put("/registrar/:id", cadastrarAnimais.update);
 
 module.exports = rotas;
