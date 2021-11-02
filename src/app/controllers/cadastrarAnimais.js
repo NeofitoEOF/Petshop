634bbea86b3db7dc2dfc8b9e36cae73741ddbed7
jose.paulo <jose.paulo@irancho.com.br>
2021-11-02 19:59:13 -0300

    feat Add controller delete animal

--- src/app/controllers/cadastrarAnimais.js
+++ src/app/controllers/cadastrarAnimais.js
@@ -44,6 +44,18 @@ class cadastrarAnimais {
       return res.status(404).json({error: 'Animal inexistente'});
     };
   }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      const animalDeletado = await cadastrarAnimaisServices.apagarAnimal(id);
+      res.status(201).json('Animal Deletado');
+    } catch (error) {
+      return res.status(404).json({error: 'Animal inexistente'});
+    }
+  }
+
+
 }
 
 module.exports = new cadastrarAnimais();
